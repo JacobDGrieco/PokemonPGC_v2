d3ec1ea288e43ffeaf25bbb55eb68d2267eb9f3a
JacobDGrieco <jacobdgrieco@gmail.com>
2026-03-10 17:13:22 -0400

    image path fix

--- frontend/data/helpers/images.js
+++ frontend/data/helpers/images.js
@@ -1,7 +1,7 @@
 
 const ITEM = ["apricorns", "balls", "berries", "decorations", "form-items", "fossils", "held-items", "key-items", "mails", "medicines", "mega-stones", "partner-gifts", "stat-items", "hms", "tms", "trs", "treasures", "usable-items", "zcrystals"];
 window._imageByGen = function (type, genKey, name) {
-	const prefix = ITEM.includes(type) ? "items/" : "";
+	const prefix = ITEM.includes(type) ? "items" : "";
 	if (type === "hms" || type === "tms" || type === "trs") type = `thms/${type}`;
 
 	let gen = "";
@@ -9,35 +9,35 @@ window._imageByGen = function (type, genKey, name) {
 		case 1:
 		case 2:
 		case 3:
-			if (type !== "thms/tms" && type !== "thms/hms") { gen = "gen1-3/"; break; }
+			if (type !== "thms/tms" && type !== "thms/hms") { gen = "gen1-3"; break; }
 		case 4:
-			if (type === "key-items" || type === "mails") { gen = "gen4/"; break; }
+			if (type === "key-items" || type === "mails") { gen = "gen4"; break; }
 		case 5:
-			if (type === "key-items" || type === "mails") { gen = "gen5/"; break; }
-			if (type === "thms/tms" || type === "thms/hms") { gen = "gen1-5/"; break; }
+			if (type === "key-items" || type === "mails") { gen = "gen5"; break; }
+			if (type === "thms/tms" || type === "thms/hms") { gen = "gen1-5"; break; }
 		case 6:
-			if (type === "key-items") { gen = "gen6/"; break; }
+			if (type === "key-items") { gen = "gen6"; break; }
 		case 7:
-			if (type === "key-items") { gen = "gen7/"; break; }
+			if (type === "key-items") { gen = "gen7"; break; }
 		case 8:
-			if (type === "key-items") { gen = "gen8/"; break; }
-			if (type === "thms/tms" || type === "thms/hms") { gen = "gen6-8/"; break; }
+			if (type === "key-items") { gen = "gen8"; break; }
+			if (type === "thms/tms" || type === "thms/hms") { gen = "gen6-8"; break; }
 			else gen = "gen4-8/"; break;
 		case 7.5:
 		case "7_2":
-			if (type === "berries" || type === "key-items") { gen = "gen7_2/"; break; }
-			else gen = "gen4-8/"; break;
+			if (type === "berries" || type === "key-items") { gen = "gen7_2"; break; }
+			else gen = "gen4-8"; break;
 		case 8.5:
-		case "8_2": gen = "gen8_2/"; break;
-		case 9: gen = "gen9/"; break;
+		case "8_2": gen = "gen8_2"; break;
+		case 9: gen = "gen9"; break;
 		case 9.5:
-		case "9_2": gen = "gen9_2/"; break;
-		case "home": gen = "home/"; break;
+		case "9_2": gen = "gen9_2"; break;
+		case "home": gen = "home"; break;
 		case 0:
 		default: gen = "";
 	}
 
-	return _assetPath(`imgs/${prefix}/${type}/${gen}${name}.png`);
+	return _assetPath(`imgs/${prefix}/${type}/${gen}/${name}.png`);
 };
 window._imageByGame = function (type, gameKey, name, bwORc) {
 	if (type && type[type.length - 1] !== 's') type = type + 's';
@@ -47,62 +47,62 @@ window._imageByGame = function (type, gameKey, name, bwORc) {
 		// Gen 1:
 		case "red":
 		case "blue":
-			game = "gen1/red-blue/" + (!bwORc ? "bw/" : "colored/"); break;
+			game = "gen1/red-blue/" + (!bwORc ? "bw" : "colored"); break;
 		case "yellow":
-			game = "gen1/yellow/" + (!bwORc ? "bw/" : "colored/"); break;
+			game = "gen1/yellow/" + (!bwORc ? "bw" : "colored"); break;
 
 		// Gen 2
 		case "gold":
-			game = "gen2/gold/"; break;
+			game = "gen2/gold"; break;
 		case "silver":
-			game = "gen2/silver/"; break;
+			game = "gen2/silver"; break;
 		case "crystal":
-			game = "gen2/crystal/"; break;
+			game = "gen2/crystal"; break;
 
 		// Gen 3
 		case "ruby":
 		case "sapphire":
 		case "emerald":
-			game = "gen3/ruby-sapphire-emerald/"; break;
+			game = "gen3/ruby-sapphire-emerald"; break;
 		case "firered":
 		case "leafgreen":
-			game = "gen3/firered-leafgreen/"; break;
+			game = "gen3/firered-leafgreen"; break;
 
 		// Gen 4
 		case "diamond":
 		case "pearl":
 		case "platinum":
-			game = "gen4/diamond-pearl-platinum/"; break;
+			game = "gen4/diamond-pearl-platinum"; break;
 		case "heartgold":
 		case "soulsilver":
-			game = "gen4/heartgold-soulsilver/"; break;
+			game = "gen4/heartgold-soulsilver"; break;
 
 		// Gen 5
 		case "black":
 		case "white":
 		case "black2":
 		case "white2":
-			game = "gen5/"; break;
+			game = "gen5"; break;
 
 		// Gen 6
 		case "x":
 		case "y":
-			game = "gen6/xy/"; break;
+			game = "gen6/xy"; break;
 		case "omegaruby":
 		case "alphasapphire":
-			game = "gen6/omegaruby-alphasapphire/"; break;
+			game = "gen6/omegaruby-alphasapphire"; break;
 
 		// Gen 7
 		case "sun":
 		case "moon":
 		case "ultrasun":
 		case "ultramoon":
-			game = "gen7/sun-moon-ultra/"; break;
+			game = "gen7/sun-moon-ultra"; break;
 
 		// Gen 7 Part 2
 		case "letsgopikachu":
 		case "letsgoeevee":
-			game = "gen7_2/"; break;
+			game = "gen7_2"; break;
 
 		// Gen 8
 		case "sword":
@@ -111,14 +111,14 @@ window._imageByGame = function (type, gameKey, name, bwORc) {
 		case "shield":
 		case "shieldioa":
 		case "shieldct":
-			game = "gen8/sword-shield/"; break;
+			game = "gen8/sword-shield"; break;
 
 		// Gen 8 Part 2
 		case "brilliantdiamond":
 		case "shiningpearl":
-			game = "gen8_2/brilliantdiamond-shiningpearl/"; break;
+			game = "gen8_2/brilliantdiamond-shiningpearl"; break;
 		case "legendsarceus":
-			game = "gen8_2/legendsarceus/"; break;
+			game = "gen8_2/legendsarceus"; break;
 
 		// Gen 9
 		case "scarlet":
@@ -127,16 +127,16 @@ window._imageByGame = function (type, gameKey, name, bwORc) {
 		case "violet":
 		case "violettm":
 		case "violetid":
-			game = "gen9/scarlet-violet/"; break;
+			game = "gen9/scarlet-violet"; break;
 
 		// Gen 9 Part 2
 		case "legendsza":
 		case "legendszamd":
-			game = "gen9_2/"; break;
+			game = "gen9_2"; break;
 
 		// HOME
 		case "home":
-			game = "home/"; break;
+			game = "home"; break;
 		default:
 			game = "";
 	}
@@ -148,20 +148,20 @@ window._ribbonByGen = function (genKey, name) {
 	switch (genKey) {
 		case 1:
 		case 2:
-		case 3: gen = "gen3/"; break;
+		case 3: gen = "gen3"; break;
 		case 4:
-		case 5: gen = "gen4-5/"; break;
+		case 5: gen = "gen4-5"; break;
 		case 6:
 		case 7:
 		case 7.5:
-		case "7_2": gen = "gen6-7/"; break;
+		case "7_2": gen = "gen6-7"; break;
 		case 8:
 		case 8.5:
 		case "8_2":
 		case 9:
 		case 9.5:
 		case "9_2":
-		case "home": gen = "gen8-9/"; break;
+		case "home": gen = "gen8-9"; break;
 		case 0:
 		default: gen = "";
 	}
